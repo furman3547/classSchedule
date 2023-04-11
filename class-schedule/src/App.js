f9f692c2ba94f913985ad77bf242b296d3f5ce4e
furman3547 <113787227+furman3547@users.noreply.github.com>
2023-04-11 18:09:51 -0500

    debug

--- class-schedule/src/App.js
+++ class-schedule/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './App.css';
+import Login from "./Components/Login"
+import Welcome from "./Components/Welcome.js"
 
 function App() {
   return (
     <div>
-      Hello! 
+      <Welcome />
+     <Login />
+     
     </div>
   );
 }
